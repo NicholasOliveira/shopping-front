cbf42f3bd2b95aa513a4f0e321837aca1ee5f0d1
Nicholas Santos <nicholas.santos.stefanini@elo.com.br>
2021-04-18 20:24:47 -0300

    feat: create context cart

---
+++ src/Contexts/Cart/index.tsx
@@ -0,0 +1,16 @@
+import { createContext, useContext } from 'react';
+import { Products } from '../../Interfaces/Products';
+import CartJSON from '../../mock/Cart.json';
+
+export type CartContextType = {
+  Cart: Products;
+  setCart: (Product: Products) => void;
+};
+
+export const CartObj = CartJSON;
+
+export const CartContext = createContext<CartContextType>({
+  Cart: CartJSON,
+  setCart: (Cart) => console.warn(Cart, 'no cart provider')
+});
+export const useCart = () => useContext(CartContext);
