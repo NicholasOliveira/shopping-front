2e710e44fe60258c0e51469952790529146feebe
Nicholas Santos <nicholas.santos.stefanini@elo.com.br>
2021-04-18 20:24:18 -0300

    feat: create file header styles

---
+++ src/Components/Header/header.styles.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 50px;
+  border-bottom: 1px solid #ccc;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+`;
