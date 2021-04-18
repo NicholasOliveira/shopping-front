64ba77e08c9e470389bda79cd5b355c9cd9a2621
Nicholas Santos <nicholas.santos.stefanini@elo.com.br>
2021-04-18 20:26:06 -0300

    feat: create home styles

---
+++ src/pages/Home/home.styles.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ProductList = styled.ul`
+  list-style: none;
+  padding: 0px;
+  margin: 0px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+`;
