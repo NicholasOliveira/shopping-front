f102e1d903990e61f4cf70da40f5b675d0d4e3d2
Nicholas Santos <nicholas.santos.stefanini@elo.com.br>
2021-04-18 20:26:21 -0300

    feat: create home test

---
+++ src/pages/Home/__tests__/Home.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Home from '../';
+
+test('renders component Home', () => {
+  render(<Home />);
+});
+
+test('renders component Home Products', () => {
+  render(<Home />);
+});
+
