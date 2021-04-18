6b1cfdf2e9f21ba473479feab0aa9308125dfff5
Nicholas Santos <nicholas.santos.stefanini@elo.com.br>
2021-04-18 20:26:39 -0300

    feat: create product item component

---
+++ src/pages/Home/Components/ProductItem/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container, ImgProduct, Title, BtnAction } from "./ProductItem.styles";
+import { Product } from "../../../../Interfaces/Products";
+
+const ProductItem: React.FC<Product> = (product: Product) => {
+  return (
+    <Container>
+      <ImgProduct src={product.img.url} />
+      <Title>{product.name}</Title>
+      <BtnAction add="add">Adicionar</BtnAction>
+    </Container>
+  );
+};
+
+export default ProductItem;
