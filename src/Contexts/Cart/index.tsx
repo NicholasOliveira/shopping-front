c3311c840e7053503d588bb6c498463062988f0a
Nicholas Santos <nicholas.santos.stefanini@elo.com.br>
2021-04-19 11:36:20 -0300

    fix: update contenxt cart

--- src/Contexts/Cart/index.tsx
+++ src/Contexts/Cart/index.tsx
@@ -1,16 +1,13 @@
-import { createContext, useContext } from "react";
-import { Products } from "../../Interfaces/Products";
-import CartJSON from "../../mock/Cart.json";
+import { createContext, useContext } from 'react';
+import { Products } from '../../Interfaces/Products';
 
 export type CartContextType = {
   Cart: Products;
   setCart: (Product: Products) => void;
 };
 
-export const CartObj = CartJSON;
-
 export const CartContext = createContext<CartContextType>({
-  Cart: CartJSON,
-  setCart: (Cart) => console.warn(Cart, "no cart provider"),
+  Cart: [],
+  setCart: (Cart) => console.warn(Cart, 'no cart provider'),
 });
 export const useCart = () => useContext(CartContext);
