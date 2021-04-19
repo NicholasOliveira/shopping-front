5128b7288e510ea2851d51d606bebada7a5a5d6a
Nicholas Santos <nicholas.santos.stefanini@elo.com.br>
2021-04-19 11:36:30 -0300

    fix: update contenxt products

--- src/Interfaces/Products/index.tsx
+++ src/Interfaces/Products/index.tsx
@@ -1,10 +1,22 @@
-export type Products = Array<Product>
+export type Products = Array<Product>;
 
 export type Product = {
-      id: number,
-      name: string,
-      img:{
-        title: string,
-        url: string,
-      }
+  id: number;
+  name: string;
+  img: {
+    title: string;
+    url: string;
+  };
+};
+
+export interface ProductFunctions extends Product {
+  handleAddCart: Function;
+  handleRemoveCart: Function;
+}
+
+export interface ProductRemoveCart extends Product {
+  handleRemoveCart: Function;
+}
+export interface ProductAddCart extends Product {
+  handleAddCart: Function;
 }
