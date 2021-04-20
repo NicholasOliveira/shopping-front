cba8a366cceadb3bdc7f01b7eceac1a2b1b8995f
Nicholas Santos <nicholas.santos.stefanini@elo.com.br>
2021-04-19 22:54:23 -0300

    refactor: home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ProductItem from './Components/ProductItem';
+import Loading from '../../Components/Loading';
 import { useProduct } from '../../Contexts/Product';
 import { Container, ProductList } from './home.styles';
+import apiData from '../../converter/apiData';
 
 const Home: React.FC = () => {
-  const { Product } = useProduct();
-
+  const [loading, setLoading] = useState(false);
+  const { Product, setProduct } = useProduct();
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        setLoading(true);
+        const result: any = await apiData();
+        setProduct(result);
+        setLoading(false);
+      } catch {
+        setLoading(false);
+      }
+    }
+    fetchProducts();
+  }, [setProduct]);
   return (
     <Container>
-      <ProductList>
-        {Product.map((Product) => (
-          <ProductItem key={Product.id} {...Product} />
-        ))}
-      </ProductList>
+      {loading ? (
+        <Loading />
+      ) : (
+        <ProductList>
+          {Product.map((Product) => (
+            <ProductItem key={Product.id} {...Product} />
+          ))}
+        </ProductList>
+      )}
     </Container>
   );
 };
