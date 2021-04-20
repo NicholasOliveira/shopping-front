ce8f635d9b2f22efcc6bd996e332435583b1ce04
Nicholas Santos <nicholas.santos.stefanini@elo.com.br>
2021-04-19 22:53:58 -0300

    refactor: add new loading in context product

--- src/Contexts/Product/index.tsx
+++ src/Contexts/Product/index.tsx
@@ -1,16 +1,18 @@
 import { createContext, useContext } from 'react';
 import { Products } from '../../Interfaces/Products';
-import ProductsJSON from '../../mock/Products.json';
 
 export type ProductContextType = {
   Product: Products;
   setProduct: (Product: Products) => void;
 };
 
-export const ProductsObj = ProductsJSON;
+export const ProductsObj = [];
 
-export const ProductContext = createContext<ProductContextType>({
-  Product: ProductsJSON,
-  setProduct: (Product) => console.warn('no products provider'),
-});
+export const ProductContext = createContext<ProductContextType>(
+  {
+    Product: [],
+    setProduct: (Product) =>
+      console.warn('no products provider'),
+  },
+);
 export const useProduct = () => useContext(ProductContext);
